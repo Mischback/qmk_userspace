5c850109aa019d7680d4d09e51b5de4eba576b36
Mischback <dev@mischback.de>
2024-02-29 15:01:52 +0100

    Disable DIP and Mouse Keys features

--- keyboards/keychron/q8/iso_encoder/keymaps/mischback/rules.mk
+++ keyboards/keychron/q8/iso_encoder/keymaps/mischback/rules.mk
@@ -1,1 +1,13 @@
 ENCODER_MAP_ENABLE = yes
+
+# The Keychron Q8 features a DIP switch at the back that let users switch
+# between Mac and Windows layouts.
+#
+# While this feature is deactivated, the default selection of layers in
+# Keychron's default keymap is not working and the first layer will be applied
+# as the default to the keyboard (not an issue, as this layer is custom anyway).
+DIP_SWITCH_ENABLE = no
+
+# The Q8's keyboard code enables Mouse Keys but that feature is not used in
+# this keymap.
+MOUSEKEY_ENABLE = no
